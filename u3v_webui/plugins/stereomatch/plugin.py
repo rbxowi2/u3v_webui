88357402c6909243ba37a2bcd37eea4f7233f057
rbxowi2 <sgra6500@gmail.com>
2026-04-27 21:41:52 +0800

    Add files via upload

---
+++ u3v_webui/plugins/stereomatch/plugin.py
@@ -0,0 +1,7 @@
+from .stereomatch import StereoMatch
+
+PLUGIN_CLASS       = StereoMatch
+PLUGIN_NAME        = "StereoMatch"
+PLUGIN_VERSION     = "1.1.0"
+PLUGIN_DESCRIPTION = "Live stereo disparity from rectified frames"
+PLUGIN_MODE        = "pipeline"
