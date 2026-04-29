fd4e6d33b0a735014a073167c1a5231cd6de52fa
rbxowi2 <sgra6500@gmail.com>
2026-04-29 13:08:44 +0800

    Delete u3v_webui/plugins/stereomatch directory

--- u3v_webui/plugins/stereomatch/plugin.py
+++
@@ -1,7 +0,0 @@
-from .stereomatch import StereoMatch
-
-PLUGIN_CLASS       = StereoMatch
-PLUGIN_NAME        = "StereoMatch"
-PLUGIN_VERSION     = "1.1.1"
-PLUGIN_DESCRIPTION = "Live stereo disparity from rectified frames"
-PLUGIN_MODE        = "pipeline"
