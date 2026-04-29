f8cec13ddfd31e447f7d93ce5b544366ad31bf59
rbxowi2 <sgra6500@gmail.com>
2026-04-29 13:08:53 +0800

    Delete u3v_webui/plugins/stereodepth directory

--- u3v_webui/plugins/stereodepth/plugin.py
+++
@@ -1,7 +0,0 @@
-from .stereodepth import StereoDepth
-
-PLUGIN_CLASS       = StereoDepth
-PLUGIN_NAME        = "StereoDepth"
-PLUGIN_VERSION     = "1.0.0"
-PLUGIN_DESCRIPTION = "Live depth map and point-cloud export from stereo rectified frames"
-PLUGIN_MODE        = "pipeline"
